a64b5a0c4dfa3933de757f5c23ce1caffa1e9106
brentonwonders <brentonwonders@gmail.com>
2016-01-23 09:45:48 -0700

    Now draws circles with onMouseDown event

--- chapter01/main.js
+++ chapter01/main.js
@@ -1,10 +1,11 @@
-$(document).ready(function() {
-  'use strict';
-  paper.install(window);
-  paper.setup(document.getElementById('mainCanvas'));
+$(document).ready(function () {
+    'use strict';
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
 
   //var c = Shape.Circle(200, 200, 50);
   //c.fillColor = 'green';
+/*
   var c;
   for(var x=25; x<400; x+=50) {
     for(var y=25; y<400; y+=50) {
@@ -12,6 +13,14 @@ $(document).ready(function() {
       c.fillColor = 'green';
     }
   }
+*/
 
-  paper.view.draw();
+    //Handling User Input
+    var tool = new Tool();
+    tool.onMouseDown = function (event) {
+        var c = Shape.Circle(event.point, 20);
+        c.fillColor = 'green';
+    };
+
+    paper.view.draw();
 });
